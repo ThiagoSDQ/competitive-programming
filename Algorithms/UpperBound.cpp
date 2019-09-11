75037e53d94709b45a7dfa737f88b2ff94a7613e
ThiagoSDQ <thiagosdqueiroz@gmail.com>
2019-09-11 15:04:47 -0300

    upperbound

---
+++ Algorithms/UpperBound.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a[100100];
+
+int upperBound(int v){
+    int l = 1, r = n;
+
+    while(l < r){
+        int m = (l + r)/2;
+
+        if(a[m] < v){
+            l = m + 1;
+        }else{
+            r = m; 
+        }
+    }
+
+    return l;
+}
