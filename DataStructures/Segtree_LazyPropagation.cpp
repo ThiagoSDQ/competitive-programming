48f275390b42229413facdc7c7faa9b60e6eaa17
Thiago Queiroz <thiagosdqueiroz@gmail.com>
2020-01-30 18:48:05 -0300

    Reduced code

--- DataStructures/Segtree_LazyPropagation.cpp
+++ DataStructures/Segtree_LazyPropagation.cpp
@@ -5,30 +5,32 @@ using namespace std;
 int segtree[400040];
 int lazy[400040];
 
-void update(int no, int i, int f, int a, int b, int valor){
+void prop(int no, int i, int f){
     int l = 2 * no, r = 2 * no + 1;
 
-    if(lazy[no] != 0){
-        segtree[no] += lazy[no] * (f - i + 1);
+    segtree[no] += lazy[no] * (f-i+1);
 
-        if(i != f){
-            lazy[l] += lazy[no];
-            lazy[r] += lazy[no];            
-        }
-
-        lazy[no] = 0;
+    if(i != f){
+        lazy[l] += lazy[no];
+        lazy[r] += lazy[no];            
     }
+
+    lazy[no] = 0;
+}
+
+void update(int no, int i, int f, int a, int b, int valor){
+    int l = 2 * no, r = 2 * no + 1, m = (i+f)/2;
+
+    if(lazy[no] != 0) prop(no, i, f);
 
     if(i > f || i > b || f < a) return ;
 
     if(a <= i && f <= b){
-        segtree[no] += valor * (f - i + 1);
-
-        lazy[l] += valor;
-        lazy[r] += valor;
+        lazy[no] += valor;
+        prop(no, i, f);
     }else{
-        update(l, i, (i+f)/2, a, b, valor);
-        update(r, (i+f)/2 + 1, f, a, b, valor);
+        update(l, i, m, a, b, valor);
+        update(r, m + 1, f, a, b, valor);
 
         segtree[no] = segtree[l] + segtree[r];
     }
@@ -36,24 +38,14 @@ void update(int no, int i, int f, int a, int b, int valor){
     return ;
 }
 
-int soma(int no, int i, int f, int a, int b){
-    int l = 2 * no, r = 2 * no + 1;
+int query(int no, int i, int f, int a, int b){
+    int l = 2 * no, r = 2 * no + 1, m = (i+f)/2;
 
-    if(lazy[no] != 0){
-        segtree[no] += lazy[no] * (f - i + 1);
-
-        if(i != f){
-            lazy[l] += lazy[no];
-            lazy[r] += lazy[no];            
-        }
-
-        lazy[no] = 0;
-    }
+    if(lazy[no] != 0) prop(no, i, f);
 
     if(i > f || i > b || f < a) return 0;
 
     if(a <= i && f <= b) return segtree[no];
     
-    return soma(l, i, (i+f)/2, a, b) + soma(r, (i+f)/2 + 1, f, a, b);
+    return query(l, i, m, a, b) + query(r, m + 1, f, a, b);
 }
-
